e3e56d65ceea66d5504fd552ff28c6cb586b4be2
Omur1ce <s2289730@ed.ac.uk>
2024-03-21 16:10:00 +0000

    mockito config

--- src/test/java/AddWebpageSystemTests.java
+++ src/test/java/AddWebpageSystemTests.java
@@ -2,14 +2,38 @@
 import org.junit.jupiter.api.Test;
 
 // class imports
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import java.util.HashMap;
 
 
 class AddWebpageSystemTests {
 
     @Test
-    void _test(){
+    void testAddNewPage(){
+        //testing when adding a non existing page
 
-        // write test here
+    }
+
+    @Test
+    void testAddExistingPageOverwrite(){
+        //testing when adding an existing page and overwriting it
+
+    }
+
+    @Test
+    void testAddExistingPageNoOverwrite(){
+        //testing when adding an existing page and not overwriting it
+
+    }
+
+    @Test
+    void emailServiceFail(){
+        //testing email service failing
 
     }
 
